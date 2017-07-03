389c097a1c2d23db118a720938c734f13e2b0949
E427632 <zhijian.xu@honeywell.com>
2017-07-03 17:45:52 +0800

    django add app story and view - index

--- web-Django/storytime/story/views.py
+++ web-Django/storytime/story/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+def index(request):
+    #request.POST
+    #request GET
+    return HttpResponse("Hello world inside index()!")
--- web-Django/storytime/storytime/urls.py
+++ web-Django/storytime/storytime/urls.py
@@ -8,14 +8,16 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_vi#ew(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
 from django.contrib import admin
+from story import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'index/', views.index),
 ]
